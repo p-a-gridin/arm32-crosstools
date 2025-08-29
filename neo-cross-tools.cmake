81378c1346b5fc5e14d94aabd6781fdb32fd4a5a
p-a-gridin <p.a.gridin@gmail.com>
2025-08-29 16:27:05 +0300

    + neo libusb

--- neo-cross-tools.cmake
+++ neo-cross-tools.cmake
@@ -29,6 +29,7 @@ include_directories(C:/SysGCC/nanopi-neo/include)
 # иногда не работает: link_directories(C:/SysGCC/nanopi-neo/lib)
 list(APPEND TBB_LIBS C:/SysGCC/nanopi-neo/lib/libtbb.so C:/SysGCC/nanopi-neo/lib/libtbbmalloc.so)
 list(APPEND MODBUS_LIB C:/SysGCC/nanopi-neo/lib/libmodbus.so.5)
+list(APPEND USB_LIB C:/SysGCC/nanopi-neo/lib/libusb-1.0.so)
 
 file(GLOB files ${CMAKE_CURRENT_LIST_DIR}/nanopi-neo/lib/libsfml*)
 list(APPEND SFML_LIBS ${files})
