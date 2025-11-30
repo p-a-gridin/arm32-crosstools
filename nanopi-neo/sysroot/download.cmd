6dc47e14ca367d2a0ed6001cf3901da4401fd459
p-a-gridin <p.a.gridin@gmail.com>
2025-11-30 12:11:33 +0300

    вытаскивание sysroot для nanopi

---
+++ nanopi-neo/sysroot/download.cmd
@@ -0,0 +1,24 @@
+rem вероятно нужны admin права для создания junction
+wsl rsync -avz --delete ^
+  --exclude=/proc ^
+  --exclude=/sys ^
+  --exclude=/dev ^
+  --exclude=/tmp ^
+  --exclude=/run ^
+  --exclude=/mnt ^
+  --exclude=/media ^
+  --exclude=/lost+found ^
+  --exclude=/bin ^
+  --exclude=/usr/bin ^
+  --exclude=/sbin ^
+  --exclude=/usr/sbin ^
+  --exclude=/usr/games ^
+  --exclude=/usr/share ^
+  --exclude=/usr/local/Trolltech ^
+  --exclude=/var ^
+  --exclude=/boot ^
+  --exclude=/etc ^
+  --exclude=/srv ^
+  --exclude=/home ^
+  --exclude=/root ^
+  pi@192.168.0.207:/ nano-pi/
